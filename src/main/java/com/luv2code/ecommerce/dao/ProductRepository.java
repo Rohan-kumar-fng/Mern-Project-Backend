ea2359d77eb0252bf7ba021b8d9898a6ce974aba
Rohan-kumar <rohan.kumar.coding@gmail.com>
2024-12-15 23:34:39 +0900

    Added MVP for Ecommerce Backend Project

---
+++ src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
@@ -0,0 +1,20 @@
+package com.luv2code.ecommerce.dao;
+
+import com.luv2code.ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin("http://localhost:4200")
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    // This will write query internally like (select * from products where category_id = id);
+    // The exposed endpoints are
+    // http://localhost:8080/api/products/search/findByCategoryId?id=1
+    // same as function name
+
+    // You can also write it in @Query field.(TODO: implement using @Query)
+    Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
+}
